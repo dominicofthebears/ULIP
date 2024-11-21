952a277c48573075085f88dd2885cb3d3ae96150
dominicofthebears <79853417+dominicofthebears@users.noreply.github.com>
2024-11-21 10:29:39 +0100

    Update misc.py

--- models/pointbert/misc.py
+++ models/pointbert/misc.py
@@ -64,8 +64,8 @@ def fps(data, number):
         data B N 3
         number int
     '''
-    fps_idx = pointnet2_utils.furthest_point_sample(data, number) 
-    fps_data = pointnet2_utils.gather_operation(data.transpose(1, 2).contiguous(), fps_idx).transpose(1,2).contiguous()
+    fps_idx = pointnet2_utils.farthest_point_sample(data, number) 
+    fps_data = pointnet2_utils.index_points(data.transpose(1, 2).contiguous(), fps_idx).transpose(1,2).contiguous()
     return fps_data
 
 def worker_init_fn(worker_id):
